9f5afc18f644e77a73baffde1b2c2006278e7c49
Tissa Khosla <tissakhosla@Tissas-MacBook-Air.local>
2019-09-21 19:15:01 -0400

    added objects

--- js/main.js
+++ js/main.js
@@ -1,19 +1,45 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		file: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		file: "images/queen-of-diamonds.png"
+	}, 
+	{
+		rank: "king",
+		suit: "hearts",
+		file: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		file: "images/king-of-diamonds.png"
+	}
+	];
+
 var cardsInPlay = [];
 
 function checkForMatch(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
 			alert("It's a Match!");
-		} else{
-			alert("Nope!");
-		}
+		} else(alert("Sorry, try again!"))
 }
 
 function flipCard(cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-	checkForMatch();	
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit)
+	console.log(cards[cardId].file);
+		
 }
 
 flipCard(0);
-flipCard(3);
+flipCard(2);
+checkForMatch();
+
+
+
