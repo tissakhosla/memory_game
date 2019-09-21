ab420cddc47cac2ca5044b380a4d7d2d50be63bc
Tissa Khosla <tissakhosla@Tissas-MacBook-Air.local>
2019-09-20 23:12:06 -0400

    created js functions

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
-//alert('Hello!');
-console.log("Up and running");
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if(cardsInPlay.length === 2){
+function checkForMatch(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("It's a Match!");
-	}
-	else{
-		alert("Nope!");
-	}
+			alert("It's a Match!");
+		} else{
+			alert("Nope!");
+		}
 }
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();	
+}
+
+flipCard(0);
+flipCard(3);
